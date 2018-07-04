e24f3b3066199f8a6bdbb803d5d51e149942b53e
Darren R. Starr <darren@nocturnal.no>
2018-07-04 03:54:21 +0200

    Moved libcdp to the parent directory to make it reusable

--- cdptools/cdp_packet_parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef CDP_PACKET_PARSER_H
-#define CDP_PACKET_PARSER_H
-
-#include "cdp.h"
-#include "stream_reader.h"
-
-int cdp_parse_packet(struct stream_reader*reader, struct s_cdp_neighbor **neighbor);
-
-#endif
